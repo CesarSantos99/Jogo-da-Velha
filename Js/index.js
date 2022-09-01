255478297ab3bde2ac7a739d0fa0b886ee4f72b6
Cesar Santos <cesarsantossouza99@gmail.com>
2022-09-01 20:27:23 -0300

    Em andamento

--- Js/index.js
+++ Js/index.js
@@ -1,16 +1,22 @@
-const celulas = document.querySelectorAll(".jogo");
+const celulas = document.querySelectorAll(".celula");
 let checarTurno = true;
 const jogador_x = "x";
 const jogador_o = "O";
 
 document.addEventListener("click", (event) => {
-    if(event.target.matches(".jogo")){
+    if(event.target.matches(".celula")){
         jogar(event.target.id);
     }
 });
 
-function jogar(){
+function jogar(id){
     const celula = document.getElementById(id);
     turno = checarTurno ? jogador_x : jogador_o;
     celula.textContent = turno;
+    checarTurno = !checarTurno;
+    checarVencedor(turno);
 };
+
+function checarVencedor(turno) {
+
+}
