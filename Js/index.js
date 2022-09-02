38596471692f5f5b0bd151e135370d06bbda4963
Cesar Santos <cesarsantossouza99@gmail.com>
2022-09-02 18:36:37 -0300

    Em andamento

--- Js/index.js
+++ Js/index.js
@@ -3,6 +3,17 @@ let checarTurno = true;
 const jogador_x = "x";
 const jogador_o = "O";
 
+const combinacoes = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
 document.addEventListener("click", (event) => {
     if(event.target.matches(".celula")){
         jogar(event.target.id);
@@ -18,5 +29,5 @@ function jogar(id){
 };
 
 function checarVencedor(turno) {
-
+    const vencedor = combinacoes.some
 }
