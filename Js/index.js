bcae55aa6b32f2fc2b34dceacf1a7ca392410df2
Cesar Santos <cesarsantossouza99@gmail.com>
2022-09-06 21:46:43 -0300

    andamento

--- Js/index.js
+++ Js/index.js
@@ -24,10 +24,45 @@ function jogar(id){
     const celula = document.getElementById(id);
     turno = checarTurno ? jogador_x : jogador_o;
     celula.textContent = turno;
-    checarTurno = !checarTurno;
+    celula.classList.add(turno);
     checarVencedor(turno);
 };
 
 function checarVencedor(turno) {
-    const vencedor = combinacoes.some
+    const vencedor = combinacoes.some((comb) => {
+        return comb.every ((index) => {
+            return celulas (index).classList.contains(turno)       
+        })
+    })
+    if(vencedor){
+        encerraJogo(turno);
+    }
+    else if (checarImpate){
+        encerraJogo();
+    }
+    else {
+        checarTurno = !checarTurno;
+    }
+}
+
+function checarImpate(){
+    let x = 0;
+    let o = 0;
+
+    for (index in celulas) {
+        if (celulas (index).classList.contains (jogador_x)) {
+            x++;
+        }
+        if (celulas (indrex).classList.contains (jogador_o)) {
+            o++;
+        }
+    }
+} 
+
+function encerraJogo (vencedor = null) {
+    if (vencedor){
+
+    } else {
+
+    }
 }
